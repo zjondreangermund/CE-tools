83129fd555f7addf35c555c5862516dfb8281061
zjondreangermund <zjondreangermund@gmail.com>
2026-07-24 06:43:35 +0200

    fix: add explicit entity and baseline type aliases

--- src/CE.Tools.Civil3D/AutoCADTypeAliases.cs
+++ src/CE.Tools.Civil3D/AutoCADTypeAliases.cs
@@ -1,2 +1,4 @@
 global using DBObject = Autodesk.AutoCAD.DatabaseServices.DBObject;
+global using Entity = Autodesk.AutoCAD.DatabaseServices.Entity;
 global using Table = Autodesk.AutoCAD.DatabaseServices.Table;
+global using Baseline = Autodesk.Civil.DatabaseServices.Baseline;
