37d809757dd9b4ceae57a838f8e4f66ca95bdd58
zjondreangermund <zjondreangermund@gmail.com>
2026-08-13 17:00:15 +0200

    Add road profile section command shell

---
+++ src/CE.Tools.Civil3D/August13RoadProfileSectionCommands.cs
@@ -0,0 +1,3 @@
+using Autodesk.AutoCAD.Runtime;
+[assembly: CommandClass(typeof(CETools.Civil3D.August13RoadProfileSectionCommands))]
+namespace CETools.Civil3D { public sealed class August13RoadProfileSectionCommands { } }
